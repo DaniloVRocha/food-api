c36d86c7a12a6c6f59b66d068ff6c5f5520a5064
Danilo Vieira <danilorocha722@gmail.com>
2022-10-15 15:21:38 -0300

    Adicionando Lombok e Mapeando Classes com JPA

---
+++ src/main/java/br/com/food/domain/model/Restaurante.java
@@ -0,0 +1,19 @@
+package br.com.food.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Restaurante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+}
