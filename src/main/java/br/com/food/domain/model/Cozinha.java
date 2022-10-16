545cfb955f27bc0d184df5d1cedee70daf4ffe07
Danilo Vieira <danilorocha722@gmail.com>
2022-10-16 18:37:08 -0300

    Adicionando JoinColumn

--- src/main/java/br/com/food/domain/model/Cozinha.java
+++ src/main/java/br/com/food/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package br.com.food.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,7 +19,8 @@ public class Cozinha {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@Column(nullable = false)
 	private String nome;
 	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
 insert into cozinha (nome) values ('Brasileira');
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Brazilians Food Gourmet', 5, 3);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Indiana J Gourmet', 1, 2);
